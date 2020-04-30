19d9760b32fb77dd8e1dfc88a0f8baf19c1b3b23
linabounsy <lina_bounsy@hotmail.fr>
2020-04-30 17:34:36 +0200

    ajout recup un post grace à l'id dans PostManager

--- model/PostManager.php
+++ model/PostManager.php
@@ -4,14 +4,25 @@ namespace Model;
 
 require_once ('model/Database.php');
 
-class Post extends Database
+class PostManager extends Database
 
 {
     public function getPosts() // recupere les derniers posts
     {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT id, title, content FROM posts');
-        return $req;
+        $req = $db->query('SELECT id, title, content FROM posts ORDER BY id DESC LIMIT 0, 3');
+        $posts = $req->fetchAll();
+        return $posts;
+
     }
+
+    public function getPost($postId) // recupere un post grace à l'id
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, title, content FROM posts WHERE id = ?');
+        $req->execute(array($postId));
+        $post = $req->fetch();
+        return $post;
+    } 
 
 }
