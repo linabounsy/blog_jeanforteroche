5aadaed720bea9a903a1e6a5a5a758e37fe86081
linabounsy <lina_bounsy@hotmail.fr>
2020-05-09 18:42:35 +0200

    connexion admin - verif login/mdp

---
+++ controller/FrontendAdmin.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Controller;
+
+use Exception;
+use Model\AdminManager;
+
+require_once('model/AdminManager.php');
+
+class FrontendAdmin
+{
+    public function adminConnexion()
+    {
+
+
+        $adminManager = new AdminManager;
+        if (isset($_POST['login']) && ($_POST['password'])) {
+            $login = $_POST["login"];
+            $password = $_POST["password"];
+
+            if (!empty($login) && !empty($password)) {
+                $user = $adminManager->adminConnexion($login);
+                
+            } else {
+                throw new Exception('Tous les champs ne sont pas remplis !');
+            }
+
+            $checkedpassword = password_verify($password, $user['password']);
+            if ($checkedpassword) {
+
+                session_start();
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['login'] = $user['login'];
+                header('Location: index.php');
+            } else {
+                throw new Exception('Mauvais login ou mot de passe');
+            }
+        }
+
+        require('view/connexionView.php');
+    
+    }
+
+        
+}
